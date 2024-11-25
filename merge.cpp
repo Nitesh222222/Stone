09eff36f7c7bd1f417fa2d877d427afc0dbfa8da
Nitesh222222 <nk6915022@gmail.com>
2024-11-25 09:55:23 +0530

    Update

---
+++ merge.cpp
@@ -0,0 +1,72 @@
+void mergeSort(int arr[], int left, int right) {
+
+if (left < right) {
+
+ int mid = (right + left) / 2;
+
+mergeSort(arr, left, mid); mergeSort(arr, mid + 1, right);
+
+ merge(arr, left, mid, right);
+
+}
+int i, j, k;
+
+int n1 = mid - left + 1;
+
+int n2 = right - mid;
+
+// Create temporary arrays
+
+int leftArr[n1], rightArr[n2];
+
+// Copy data to temporary arrays for (i = 0; i < n1; i++)
+
+leftArr[i] = arr[left + i]; for (j = 0; j < n2; j++)
+
+rightArr[j] = arr[mid + 1 + j];
+into arr[left..right]
+
+i = 0;
+
+j = 0;
+
+k = left;
+
+while (i < n1 && j < n2) {
+
+if (leftArr[i] <= rightArr[j]) {
+
+arr[k] = leftArr[i];
+
+i++;
+
+}
+
+else {
+
+arr[k] = rightArr[j];
+
+j++;
+
+}
+
+k++;
+
+}
+while (i < n1) { arr[k] = leftArr[i];
+
+i++;
+
+k++;
+
+}
+
+// Copy the remaining elements of rightArr[], if any while (j < n2) { arr[k] = rightArr[j];
+
+j++;
+
+k++;
+
+}
+
+}
